41568d57b7106adaf6d4e6e10808f68da148df54
Cara R <cara.rosa.02@gmail.com>
2023-09-11 16:13:31 -0500

    added my API key, a variable for the city, and a request URL

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const APIKey = "8ffcaf78a4d3963d8cfc46aad122cce3";
+var city;
+var requestURL = "https://api.openweathermap.org/data/2.5/forecast?lat=30.26&lon=-97.3&appid=8ffcaf78a4d3963d8cfc46aad122cce3";
+
+
+
