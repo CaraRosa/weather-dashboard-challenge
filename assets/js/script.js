a3612abacbb5807f1accc38d49a5752e00ca710a
Cara R <cara.rosa.02@gmail.com>
2023-09-12 15:58:10 -0500

    added an event listener to the submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 const APIKey = "8ffcaf78a4d3963d8cfc46aad122cce3";
-var city;
-var requestURL = "https://api.openweathermap.org/data/2.5/forecast?lat=30.26&lon=-97.3&appid=8ffcaf78a4d3963d8cfc46aad122cce3";
+let requestURL = "https://api.openweathermap.org/data/2.5/forecast?lat=30.26&lon=-97.3&appid=8ffcaf78a4d3963d8cfc46aad122cce3";
+
+
+// Accessing elements by ID
+const submitBtn = document.querySelector("#submit-btn");
+
+document.getElementById("submit-btn").addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("click submit button");
+})
 
 
 
