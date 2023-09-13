f244ea60b44bdc9042cc7b38f2eebf53bd5fd816
Cara R <cara.rosa.02@gmail.com>
2023-09-13 16:38:40 -0500

    added code to render user's search history when the page is reloaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,52 @@
-const APIKey = "8ffcaf78a4d3963d8cfc46aad122cce3";
-let requestURL = "https://api.openweathermap.org/data/2.5/forecast?lat=30.26&lon=-97.3&appid=8ffcaf78a4d3963d8cfc46aad122cce3";
+const APIKey = '8ffcaf78a4d3963d8cfc46aad122cce3';
+let requestURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=30.26&lon=-97.3&appid=8ffcaf78a4d3963d8cfc46aad122cce3';
 
 
 // Accessing elements by ID
-const submitBtn = document.querySelector("#submit-btn");
+const submitBtn = document.querySelector('#submit-btn');
 
-// Saving user input to local storage
-if (localStorage.getItem("userInput")) {
-    var userInputHistory = JSON.parse(localStorage.getItem("userInput"));
-} else {
-    var userInputHistory = [];
+// renders user input
+function renderUserInput() {
+    var userInputHistory = JSON.parse(localStorage.getItem('userInput'));
+    var searchList = document.getElementById('searchList');
+
+    if (userInputHistory !== null) {
+        searchList.innerHTML = '';
+
+        userInputHistory.forEach(function (userInput) {
+            var listSearchItem = document.createElement('p');
+            listSearchItem.textContent = userInput.userInput;
+            searchList.appendChild(listSearchItem);
+        });
+    }
 }
 
-console.log(typeof userInputHistory);
+// event listener for the DOMContentLoaded event
+document.addEventListener('DOMContentLoaded', function () {
+    // Call the renderUserInput function to render user input when the page loads
+    renderUserInput();
 
-document.getElementById("submit-btn").addEventListener("click", function(event) {
-    // prevents the form from submitting
-    event.preventDefault();
+    // event listener for the submit button
+    document.getElementById('submit-btn').addEventListener('click', function (event) {
+        event.preventDefault();
 
-    var search = document.getElementById("search-input").value;
-    
-    var userInput = {
-        userInput: search,
-    }
+        var search = document.getElementById('search-input').value;
 
-    if (search.trim() !== "") {
-        userInputHistory.push(userInput);
-        localStorage.setItem("userInput", JSON.stringify(userInputHistory));
-        console.log("user search", search);
-    } else {
-        console.log("Please enter a city.");
-    }
+        var userInput = {
+            userInput: search,
+        };
 
-    console.log("click");
+        if (search.trim() !== '') {
+            var userInputHistory = JSON.parse(localStorage.getItem('userInput')) || [];
+            userInputHistory.push(userInput);
+            localStorage.setItem('userInput', JSON.stringify(userInputHistory));
+
+            // After adding new input, re-render the user input list
+            // After the user enters a new input, it again renders users' history of cities that they have searched for
+            renderUserInput();
+        } else {
+            console.error('You need to enter a city name.');
+            return;
+        }
+    });
 });
-
-// Renders user input to the webpage
-document.getElementById("submit-btn").addEventListener("click", function(event) {
-var userInputHistory = JSON.parse(localStorage.getItem("userInput"));
-var searchList = document.getElementById("searchList");
-
-if (userInputHistory !== null) {
-    searchList.innerHTML = "";
-
-    userInputHistory.forEach(function (userInput) {
-        var listSearchItem = document.createElement("p");
-        listSearchItem.textContent = userInput.userInput;
-        searchList.appendChild(listSearchItem);
-    })
-}
-
-});
-
-
-
-
-
