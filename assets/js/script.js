d4d20319f19c798df1545133cdaba0fcab6748f9
Cara R <cara.rosa.02@gmail.com>
2023-09-13 12:27:49 -0500

    added local storage for user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,38 @@ let requestURL = "https://api.openweathermap.org/data/2.5/forecast?lat=30.26&lon
 // Accessing elements by ID
 const submitBtn = document.querySelector("#submit-btn");
 
+if (localStorage.getItem("userSearch")) {
+    var userSearchHistory = JSON.parse(localStorage.getItem("userSearch"));
+} else {
+    var userSearchHistory = [];
+}
+
+console.log(typeof userSearchHistory);
+
 document.getElementById("submit-btn").addEventListener("click", function(event) {
+    // prevents the form from submitting
     event.preventDefault();
+
+
+    var userInput = document.getElementById('search-input').value;
+
+    var userSearch = {
+        userSearch: userInput,  
+    }
+
+    if(userInput.trim() !== "") {
+        userSearchHistory.push(userSearch);
+        localStorage.setItem("userSearch", JSON.stringify(userSearchHistory));
+        console.log("user search", userInput);
+    } else {
+        console.log("Please enter a city.");
+    }
+
     console.log("click submit button");
-})
+
+
+});
+
 
 
 
