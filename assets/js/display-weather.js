4bc9869396871e4b8b8f584ca59dd89438d9dc35
Cara R <cara.rosa.02@gmail.com>
2023-09-14 00:00:58 -0500

    edited file

--- assets/js/display-weather.js
+++ assets/js/display-weather.js
@@ -1,2 +1,24 @@
-var weatherResultEl = document.querySelector('weather-result');
+const baseURL = 'https://api.openweathermap.org/data/2.5/forecast';
+const APIKey = '8ffcaf78a4d3963d8cfc46aad122cce3';
+const weatherResultEl = document.querySelector('#weather-result');
+
+var getWeatherData = function(latitude, longitude) {
+    var apiUrl = baseURL + '?lat=' + latitude + '&lon=' + longitude + APIKey;
+
+    fetch(apiUrl)
+        .then(function (response) {
+            if(response.ok) {
+                return response.json(); 
+            } else {
+                throw new Error('Error: ' + response.statusText);
+            }
+        })
+        .then(function (data) {
+            // displayWeather(data, longitude, latitude);
+            console.log(data);
+        })
+        .catch(function (error) {
+            alert('Unable to connect to Open Weather:' + error.message);
+        });
+};
 
