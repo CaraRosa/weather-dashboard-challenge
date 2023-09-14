b44fc12e7ba971b4ca56290a5ea41962a4b1bdbc
Cara R <cara.rosa.02@gmail.com>
2023-09-14 08:29:21 -0500

    added code to loop through the array by targeting a time of 12 PM for 5 days

--- assets/js/display-weather.js
+++ assets/js/display-weather.js
@@ -1,9 +1,9 @@
 const baseURL = 'https://api.openweathermap.org/data/2.5/forecast';
-const APIKey = '8ffcaf78a4d3963d8cfc46aad122cce3';
+// const APIKey = '8ffcaf78a4d3963d8cfc46aad122cce3';
 const weatherResultEl = document.querySelector('#weather-result');
 
-var getWeatherData = function(latitude, longitude) {
-    var apiUrl = baseURL + '?lat=' + latitude + '&lon=' + longitude + APIKey;
+var getWeatherData = function(cityName) {
+    var apiUrl = baseURL + '?q=' + cityName + '&appid=' + APIKey;
 
     fetch(apiUrl)
         .then(function (response) {
@@ -15,7 +15,20 @@ var getWeatherData = function(latitude, longitude) {
         })
         .then(function (data) {
             // displayWeather(data, longitude, latitude);
-            console.log(data);
+            console.log(data.list);
+
+            for (let index = 0; index < data.list.length; index++) {
+                const element = data.list[index];
+                // console.log(element);
+                
+
+                if (element.dt_txt.endsWith('12:00:00')) {
+                    console.log(element);
+                    // element.main.temp
+                    // appendChild or innerHTML to display results
+                }
+            }
+
         })
         .catch(function (error) {
             alert('Unable to connect to Open Weather:' + error.message);
