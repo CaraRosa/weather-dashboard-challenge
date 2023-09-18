dfbe9c6ba364e7db1533e726493b55cdd8de7d38
Cara R <cara.rosa.02@gmail.com>
2023-09-18 16:02:06 -0500

    added code to create cards for the 5 future days specifying the weather

--- assets/js/display-weather.js
+++ assets/js/display-weather.js
@@ -1,11 +1,14 @@
 const baseURL = 'https://api.openweathermap.org/data/2.5/forecast';
 // const APIKey = '8ffcaf78a4d3963d8cfc46aad122cce3';
 const weatherResultEl = document.querySelector('#weather-result');
+const weatherContainer = document.querySelector('#weather-container');
+
 
 // converts temperature
 function convertTemp(kelvin) {
     const celsius = kelvin - 273.15;
     const fahrenheit = (celsius * 9/5) + 32;
+    // sets the decimal place to 2
     return fahrenheit.toFixed(2);
 }
 
@@ -17,6 +20,7 @@ function getCurrentDate() {
     const day = String(now.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
 }
+
 
 var getWeatherData = function(cityName) {
     var apiUrl = baseURL + '?q=' + cityName + '&appid=' + APIKey;
@@ -30,34 +34,54 @@ var getWeatherData = function(cityName) {
             }
         })
         .then(function (data) {
-            var html = '';
-            const currentDate = getCurrentDate();
-            // displayWeather(data, longitude, latitude);
-            console.log(data.list);
+            // store the weather data for future days
+            var futureWeather = []; 
 
-            for (let index = 0; index < data.list.length; index++) {
-                const element = data.list[index];
-                // console.log(element);
-                
-
-                if (element.dt_txt.endsWith('12:00:00')) {
-                    console.log(element);
-                    const date = element.dt_txt.split(' ')[0];
+            // Filter data for the current date and future dates
+            data.list.forEach(function (element) {
+                const date = element.dt_txt.split(' ')[0];
+                if (isFutureDate(date) && futureWeather.length < 5) {
                     const temperatureKelvin = element.main.temp;
                     const temperatureFahrenheit = convertTemp(temperatureKelvin);
                     const wind = element.wind.speed;
                     const humidity = element.main.humidity;
-                    html += `<p>Date: ${date}, Temperature: ${temperatureFahrenheit}°F, Wind: ${wind}, Humidity: ${humidity} </p>`;
-                    // element.main.temp
-                    // appendChild or innerHTML to display results
-                    if (date === currentDate) {
-                        html += `<p>Date: ${date}, Temperature: ${temperatureFahrenheit}°F, Wind: ${wind}, Humidity: ${humidity} </p>`;
-                    }
+                    futureWeather.push({
+                        date: date,
+                        temperature: temperatureFahrenheit,
+                        wind: wind,
+                        humidity: humidity,
+                    });
                 }
-            }
-            weatherResultEl.innerHTML = html;
+            });
+            // display the weather for the future days
+            displayWeather(futureWeather); 
         })
         .catch(function (error) {
             alert('Unable to connect to Open Weather:' + error.message);
         });
 };
+
+function isFutureDate(date) {
+    const currentDate = new Date();
+    const targetDate = new Date(date);
+    return targetDate > currentDate;
+}
+
+function displayWeather(futureWeather) {
+    const weatherContainer = document.querySelector('#weather-container');
+    // clear previous weather
+    weatherContainer.innerHTML = '';
+
+    // this code makes a card for each of the 5 future days
+    futureWeather.forEach(function (weather) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+            <h3>Date: ${weather.date}</h3>
+            <p>Temperature: ${weather.temperature}°F</p>
+            <p>Wind: ${weather.wind}MPH</p>
+            <p>Humidity: ${weather.humidity} %</p>
+        `;
+        weatherContainer.appendChild(card);
+    });
+}
